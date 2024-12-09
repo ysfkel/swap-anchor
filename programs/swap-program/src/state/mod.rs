35e966615365dd77f3a27b41f6e7c5ff7993550e
ysfkel <yusufkel@gmail.com>
2024-12-09 18:52:48 +0400

    added pool

--- programs/swap-program/src/state/mod.rs
+++ programs/swap-program/src/state/mod.rs
@@ -2,6 +2,7 @@ mod pair;
 mod state;
 mod config;
 mod admin;
+mod pool;
 
 pub use state::*;
 pub use config::*;
---
+++ programs/swap-program/src/state/pool.rs
@@ -0,0 +1,131 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::Mint;
+use std::ops::{BitAnd, BitOr, BitXor};
+/// Seed to derive account address and signature
+pub const POOL_SEED: &str = "pool";
+pub const POOL_LP_MINT_SEED: &str = "pool_lp_mint";
+pub const POOL_VAULT_SEED: &str = "pool_vault";
+
+pub const Q32: u128 = (u32::MAX as u128) + 1;
+
+pub enum PoolOperation {
+    Deposit,
+    Withdraw, 
+    Swap,
+}
+
+#[derive(PartialEq, Eq)]
+pub enum PoolOperationStatus {
+    Enable,
+    Disable
+}
+
+
+pub struct Pool {
+    pub amm_config: Pubkey,
+    
+    pub pool_creator: Pubkey,
+
+    pub token_0_vault: Pubkey,
+
+    pub token_1_vault: Pubkey,
+
+    pub lp_mint: Pubkey,
+
+    pub token_0_mint: Pubkey,
+
+    pub token_1_mint: Pubkey,
+
+    pub token_0_program: Pubkey,
+
+    pub token_1_program: Pubkey,
+
+    pub observation_key: Pubkey,
+
+    pub auth_bump: u8,
+    /// Bitwise representation of the state of the pool
+    /// bit0, 1: disable deposit(vaule is 1), 0: normal
+    /// bit1, 1: disable withdraw(vaule is 2), 0: normal
+    /// bit2, 1: disable swap(vaule is 4), 0: normal
+    pub status: u8, 
+
+    pub lp_mint_decimals: u8,
+
+    pub token_0_mint_decimals: u8,
+
+    pub token_1_mint_decimals: u8,
+
+    /// True circulating supply without burns and lock ups
+    pub lp_supply: u64,
+
+    pub protocol_fees_token_0: u64,
+    pub protocol_fees_token_1: u64,
+
+    pub fund_fees_token_0: u64,
+    pub fund_fees_token_1: u64,
+
+    pub open_time: u64,
+
+    pub recent_epoch: u64,
+
+    pub padding: [u64; 31],
+}
+impl Pool {
+    fn new(&mut self,
+        auth_bump: u8,
+        lp_supply: u64,
+        open_time: u64,
+        pool_creator: Pubkey,
+        amm_config: Pubkey,
+        token_0_vault: Pubkey,
+        token_1_vault: Pubkey,
+        token_0_mint: &InterfaceAccount<Mint>,
+        token_1_mint: &InterfaceAccount<Mint>,
+        lp_mint: &InterfaceAccount<Mint>,
+        observation_key: Pubkey,
+     ) {
+        self.amm_config = amm_config.key();
+        self.pool_creator = pool_creator.key();
+        self.token_0_vault = token_0_vault;
+        self.token_1_vault = token_1_vault;
+        self.lp_mint = lp_mint.key();
+        self.token_0_mint = token_0_mint.key();
+        self.token_1_mint = token_1_mint.key();
+        self.token_0_program = *token_0_mint.to_account_info().owner;
+        self.token_1_program = *token_1_mint.to_account_info().owner;
+        self.observation_key = observation_key;
+        self.auth_bump = auth_bump;
+        self.lp_mint_decimals = lp_mint.decimals;
+        self.token_0_mint_decimals = token_0_mint.decimals;
+        self.token_1_mint_decimals = token_1_mint.decimals;
+        self.lp_supply = lp_supply;
+        self.protocol_fees_token_0 = 0;
+        self.protocol_fees_token_1 = 0;
+        self.fund_fees_token_0 = 0;
+        self.fund_fees_token_1 = 0;
+        self.open_time = open_time;
+        self.recent_epoch = Clock::get().unwrap().epoch;
+        self.padding = [0u64;31];
+    }
+
+    pub fn set_status(&mut self, status: u8) {
+        self.status = status;
+    }
+
+    pub fn set_status_by_byt(&mut self, pool_operation: PoolOperation, status: PoolOperationStatus) {
+        let pool_operation_bit = u8::from(1) << (pool_operation as u8);
+
+        match status {
+            PoolOperationStatus::Disable => {
+                self.status = self.status.bitor(pool_operation_bit);
+            },
+            PoolOperationStatus::Enable => {
+                let mask = u8::from(255).bitxor(pool_operation_bit);
+                self.status = self.status.bitand(mask);
+
+            }
+        }
+    }
+
+
+}
